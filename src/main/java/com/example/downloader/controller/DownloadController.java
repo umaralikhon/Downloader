6ddaba91c672e0b63c52cd6e1a9a28c1fddd81f1
Umaralikhon <umaralikhon@gmail.com>
2022-01-07 22:48:44 +0500

    Initial commit

--- src/main/java/com/example/downloader/controller/DownloadController.java
+++ src/main/java/com/example/downloader/controller/DownloadController.java
@@ -1,2 +1,53 @@
-package com.example.downloader.controller;public class RestController {
+package com.example.downloader.controller;
+
+import com.example.downloader.utils.MediaTypeUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@RestController
+@RequestMapping("/api")
+public class DownloadController {
+    private final ServletContext servletContext;
+
+    @Autowired
+    public DownloadController(ServletContext servletContext){
+        this.servletContext = servletContext;
+    }
+
+    private static final String PATH = "files/";
+
+    @GetMapping("/download/{fileName}")
+    public void download(@PathVariable String fileName, HttpServletResponse response) throws IOException {
+        MediaType mediaType = MediaTypeUtils.getMediaTypeForFileName(servletContext, fileName);
+        File file = new File(PATH + fileName);
+
+        response.setContentType(mediaType.getType());
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
+        response.setContentLength((int)file.length());
+
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+        BufferedOutputStream bos = new BufferedOutputStream(response.getOutputStream());
+
+        byte[] buffer = new byte[1024];
+        int bytesRead = 0;
+
+        while((bytesRead = bis.read(buffer))!=-1){
+            bos.write(buffer, 0, bytesRead);
+        }
+
+        bos.flush();
+        bis.close();
+
+    }
+
+
 }
--- src/main/java/com/example/downloader/utils/MediaTypeUtils.java
+++ src/main/java/com/example/downloader/utils/MediaTypeUtils.java
@@ -1,2 +1,18 @@
-package com.example.downloader.utils;public class MediaTypeUtils {
+package com.example.downloader.utils;
+
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletContext;
+
+public class MediaTypeUtils {
+
+    public static MediaType getMediaTypeForFileName(ServletContext servletContext, String fileName){
+        String fileType = servletContext.getMimeType(fileName);
+        try{
+            MediaType mediaType = MediaType.parseMediaType(fileType);
+            return mediaType;
+        }catch (Exception ex){
+            return MediaType.APPLICATION_OCTET_STREAM;
+        }
+    }
 }
