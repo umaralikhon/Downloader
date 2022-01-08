a936b1353b38e01332be0e4e7fbd4521507eb4ab
Umaralikhon <umaralikhon@gmail.com>
2022-01-08 07:50:27 +0500

    Initial commit

--- src/main/java/com/example/downloader/controller/DownloadController.java
+++ src/main/java/com/example/downloader/controller/DownloadController.java
@@ -2,8 +2,11 @@ package com.example.downloader.controller;
 
 import com.example.downloader.utils.MediaTypeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api")
@@ -19,7 +25,7 @@ public class DownloadController {
     private final ServletContext servletContext;
 
     @Autowired
-    public DownloadController(ServletContext servletContext){
+    public DownloadController(ServletContext servletContext) {
         this.servletContext = servletContext;
     }
 
@@ -32,7 +38,7 @@ public class DownloadController {
 
         response.setContentType(mediaType.getType());
         response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName());
-        response.setContentLength((int)file.length());
+        response.setContentLength((int) file.length());
 
         BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
         BufferedOutputStream bos = new BufferedOutputStream(response.getOutputStream());
@@ -40,14 +46,40 @@ public class DownloadController {
         byte[] buffer = new byte[1024];
         int bytesRead = 0;
 
-        while((bytesRead = bis.read(buffer))!=-1){
+        while ((bytesRead = bis.read(buffer)) != -1) {
             bos.write(buffer, 0, bytesRead);
         }
 
         bos.flush();
         bis.close();
-
     }
 
+    @GetMapping("/download/2/{filename}")
+    public ResponseEntity<ByteArrayResource> download(@PathVariable String filename) throws IOException {
+        MediaType mediaType = MediaTypeUtils.getMediaTypeForFileName(servletContext, filename);
+        Path path = Paths.get(PATH + filename);
+
+        byte[] data = Files.readAllBytes(path);
+        ByteArrayResource resource = new ByteArrayResource(data);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + path.getFileName().toString())
+                .contentType(mediaType)
+                .contentLength(data.length)
+                .body(resource);
+    }
+
+    @GetMapping("/download/3/{filename}")
+    public ResponseEntity<InputStreamResource> downloadFile(@PathVariable String filename) throws FileNotFoundException {
+        MediaType mediaType = MediaTypeUtils.getMediaTypeForFileName(servletContext, filename);
+        File file = new File(PATH + filename);
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName())
+                .contentType(mediaType)
+                .contentLength(file.length())
+                .body(resource);
+    }
 
 }
